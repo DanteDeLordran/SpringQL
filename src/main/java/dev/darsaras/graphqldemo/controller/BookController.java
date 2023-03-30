3c0e4b48358a76d11a2443e675d2e8b73b3e20dc
Lordran <dantelopez305@outlook.com>
2023-03-30 15:49:35 -0600

    No need to have @RestController nor @RequestMapping

--- src/main/java/dev/darsaras/graphqldemo/controller/BookController.java
+++ src/main/java/dev/darsaras/graphqldemo/controller/BookController.java
@@ -5,16 +5,13 @@ import java.util.List;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.stereotype.Controller;
 import dev.darsaras.graphqldemo.model.Book;
 import dev.darsaras.graphqldemo.model.BookInput;
 import dev.darsaras.graphqldemo.service.BookService;
 import lombok.RequiredArgsConstructor;
 
-@RestController
-@RequestMapping("/api/books")
+@Controller
 @RequiredArgsConstructor
 public class BookController {
 
